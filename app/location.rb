347fe26c81025f6bb8e93452fceea4ae4916a5ea
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-08 20:11:47 -0400

    learned about factorybot

--- app/location.rb
+++ app/location.rb
@@ -5,4 +5,13 @@ class Location < ActiveRecord::Base
   # Location must be in the United States
   validates :lat, inclusion: 24.7433195..49.3457868
   validates :long, inclusion: -124.7844079..-66.9513812
+
+  FactoryBot.define do
+    factory :location do
+      city "Philly"
+      state "PA"
+      lat 24.7433195
+      long -124.7844079
+    end
+  end
 end
