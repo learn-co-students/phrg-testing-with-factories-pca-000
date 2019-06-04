7985e71a9a76c346520022a28e1ffb00eb9cfd90
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-04 13:59:53 -0400

    done

---
+++ spec/factories/stork.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :stork do
+    color "Jay-Z Blue"
+    zoo
+    bill_length 12
+    wing_span 11
+  end
+end
---
+++ spec/factories/tiger.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :tiger do
+    weight "big boy"
+    zoo
+    number_of_stripes 300
+  end
+end
